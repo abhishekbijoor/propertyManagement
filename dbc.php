b2880154060305c67ca5720a9c12c9338cbfcbaf
abhishekbijoor <42459892+abhishekbijoor@users.noreply.github.com>
2019-03-12 19:32:59 +0530

    Delete dbc.php

--- dbc.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "connecting";
-$conn=mysqli_connect('localhost','root','abhishek!123','myproperty');
-//testing the connection
-if (mysqli_connect_errno()) {
-echo "DB connection error:".mysqli.maxdb_connect_error();
-}
